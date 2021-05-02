22285534bdbc4c711f2f5e5ecff54320802c695d
Ningning Zhu <nz14@rice.edu>
2021-05-02 14:20:09 -0500

    created reusable button component with styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -28,7 +29,7 @@ class SignIn extends React.Component {
       render() {
             return (
                   <div className = 'sign-in'>
-                        <h1>I already have an account</h1>
+                        <h2>I already have an account</h2>
                         <span>Sign in with your email and password.</span>
                         <form onSubmit = {this.handleSubmit}>
                               <FormInput 
@@ -47,7 +48,7 @@ class SignIn extends React.Component {
                                     label = 'Password'
                                     required />
                         
-                              <input value = 'Submit Form' type = 'submit' />
+                              <CustomButton type = 'submit'> Sign in </CustomButton>
                         </form>
                   </div>
             )

5d29e044f0fd815f34c04c705f00b485d36e1ca2
Ningning Zhu <nz14@rice.edu>
2021-05-04 12:45:28 -0500

    storing user data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { useReducer } from 'react';
 
 
 const config = {
@@ -12,6 +13,34 @@ const config = {
       appId: "1:612604851951:web:a1a3e06640853bb4eb0d1a",
       measurementId: "G-YKC8CZN9NQ"
     };
+
+export const createUserProfileDocument = async (userAuth, ...additionalData) => {
+      if(!userAuth) return;
+      
+      const userRef= firestore.doc(`users/${userAuth.uid}`);
+      const snapShot = await userRef.get();
+
+      if(!snapShot.exists) {
+            const {displayName, email} = userAuth;
+            const createdAt = new Date();
+
+            try {
+                  await userRef.set(
+                        {
+                              displayName,
+                              email,
+                              createdAt,
+                              ...additionalData
+                        }
+                  )
+            }
+            catch (error) {
+                  console.log('error creating user', error.message);
+            }
+      }
+};
+
+
 
 firebase.initializeApp(config);
 
